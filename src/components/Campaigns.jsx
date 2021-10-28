fe10a17d2afcd0aac973648a420797c350af4bd0
francosorbello <franco.sorbelo@gmail.com>
2021-10-28 11:03:44 -0300

    added add,edit and delete buttons to campaigns table

--- src/components/Campaigns.jsx
+++ src/components/Campaigns.jsx
@@ -1,20 +1,54 @@
-import { Title } from '@mui/icons-material'
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { Add, Delete, Edit, Title } from '@mui/icons-material'
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button, IconButton, Fab } from '@mui/material'
 import React, { useContext } from 'react'
 import { CampaignContext } from '../contexts/CampaignContext'
 import { DataGrid } from '@mui/x-data-grid';
+
+const style = {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+};
+
 const columns = [
     // {field: "id",headerName: "ID", width: 130},
     {field: "name",headerName: "Nombre", width: 130},
     {field: "description",headerName: "Descripcion", width: 130},
     {field: "initialDate",headerName: "Fecha Inicio", width: 130},
     {field: "endDate",headerName: "Fecha Fin", width: 130},
+    {
+        field: "edit", 
+        headerName: "Editar",
+        width:80,
+        renderCell: (cellValues) => {
+            return(
+                <IconButton aria-label="Editar">
+                    <Edit></Edit>
+                </IconButton>
+            )
+        }
+    },
+    {
+        field: "delete", 
+        headerName: "Borrar",
+        width:80,
+        renderCell: (cellValues) => {
+            return(
+                <IconButton aria-label="Editar">
+                    <Delete></Delete>
+                </IconButton>
+            )
+        }
+    }
 ]
 
 const Campaigns = () => {
     const {campaigns} = useContext(CampaignContext);
     return (
-        <div style={{display: "flex"}}>
+        <div>
             <DataGrid
                 autoHeight
                 rows={campaigns}
@@ -23,32 +57,10 @@ const Campaigns = () => {
                 rowsPerPageOptions={[5]}
             >
             </DataGrid>
+            <Fab color="primary" aria-label="add" style={style}>
+                <Add />
+            </Fab>
         </div>
-        // <div>
-        //     <TableContainer component={Paper}>
-        //         <Table>
-        //             <TableHead>
-        //                 <TableRow>
-        //                     <TableCell>Nombre</TableCell>
-        //                     <TableCell>Descripcion</TableCell>
-        //                     <TableCell>Fecha inicio</TableCell>
-        //                     <TableCell>Fecha fin</TableCell>
-        //                     {/* TODO: añadir botones de borrar/editar */}
-        //                 </TableRow>
-        //             </TableHead>
-        //             <TableBody>
-        //                 {campaigns.map((campaign)=> (
-        //                     <TableRow>
-        //                         <TableCell>{campaign.name}</TableCell>
-        //                         <TableCell>{campaign.description}</TableCell>
-        //                         <TableCell>{campaign.initialDate}</TableCell>
-        //                         <TableCell>{campaign.endDate}</TableCell>
-        //                     </TableRow>
-        //                 ))}
-        //             </TableBody>
-        //         </Table>
-        //     </TableContainer>
-        // </div>
     )
 }
 
