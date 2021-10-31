12557ac0c3d31fe99cd4f811d8ff7c1cd081905b
francosorbello <franco.sorbelo@gmail.com>
2021-10-31 19:01:25 -0300

    added campaign, status and address to donation form

--- src/components/donations/DonationForm.jsx
+++ src/components/donations/DonationForm.jsx
@@ -1,18 +1,29 @@
-import { Autocomplete, Grid, TextField } from '@mui/material'
-import React, { useContext } from 'react'
+import { Autocomplete, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/material'
+import React, { useContext, useState } from 'react'
 import { CampaignContext } from '../../contexts/CampaignContext'
 import { DonationContext } from '../../contexts/DonationContext'
 
 const DonationForm = () => {
-    const {donations,setDonations} = useContext(DonationContext)
+    const {donations,setDonations,donationStatus} = useContext(DonationContext)
     const {campaigns} = useContext(CampaignContext)
+    const [address,setAddress] = useState("")
+    const [status, setStatus] = useState("")
     
     return (
         <div>
             <form autoComplete="off">
                 <Grid container spacing={3} sx={{marginTop: 1}}>
                     <Grid item sm={12} md={12} lg={12}>
+                        <TextField
+                            label="Dirección"
+                            fullWidth
+                            value={address}
+                            onChange={(e)=>setAddress(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item sm={6} md={6} lg={6}>
                         <Autocomplete
+                            autoHighlight
                             id="select-campaign"
                             options={campaigns}
                             getOptionLabel={(option)=>option.name}
@@ -21,7 +32,25 @@ const DonationForm = () => {
                         >
                         </Autocomplete>
                     </Grid>
-                    
+                    <Grid item xs={6} sm={6} md={6}>
+                        <FormControl fullWidth>
+                            <InputLabel id="status-name-label">Status</InputLabel>
+                            <Select
+                                fullWidth = {true}
+                                labelId="status-name-label"
+                                label="Status"
+                                id="status-name-select"
+                                onChange={(e)=>setStatus(e.target.value)}
+                                value={status}
+                            >
+                                {
+                                    donationStatus.map(
+                                        ({0: id,1: name}) => <MenuItem key={id} value={id}>{name}</MenuItem>
+                                    )
+                                }
+                            </Select>
+                        </FormControl>
+                    </Grid>
                 </Grid>
             </form>        
         </div>
