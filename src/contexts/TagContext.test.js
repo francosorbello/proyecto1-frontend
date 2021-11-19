f18192a7ee192cec90d4e963d74611055cdc7262
francosorbello <franco.sorbelo@gmail.com>
2021-11-19 12:24:10 -0300

    add tag context test that shows the async problem with updating state in useEffect

--- src/contexts/TagContext.test.js
+++ src/contexts/TagContext.test.js
@@ -1,20 +1,23 @@
 import TagContextProvider, { TagContext } from "./TagContext";
 import { useContext } from "react";
 import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { act } from "react-dom/test-utils";
 
 require('jest-fetch-mock').enableMocks()
+
 
 beforeEach(()=>{
     fetch.resetMocks();
 })
 
 describe("Tag Context",()=>{
-    it("Setea lista de tags correctamente",()=>{
+    it("Setea lista de tags correctamente",async ()=>{
         fetch.mockResponseOnce(JSON.stringify([{"id":1,"name":"invierno"},{"id":2,"name":"verano"}]))
         // componente dummy que renderiza la lista de tags
         const TestComponent = () => {
             const {tags} = useContext(TagContext)
-            console.log(tags)
+            console.log("tags",tags)
             return(
                 <div>
                     <div data-testid="tag">{tags.count > 0}</div>
@@ -26,6 +29,7 @@ describe("Tag Context",()=>{
                 <TestComponent />
             </TagContextProvider>
         )
-
+        
+        expect(wrapper.getByTestId("tag")).toHaveValue("true")
     })
 })
